5e9ad28d061bb1e28f52f9e83d157854f8c31b4a
Anton Kit <croixua@gmail.com>
2022-07-11 23:23:30 +0300

    first worked version

---
+++ READMe.md
@@ -0,0 +1,5 @@
+- [https://ibb.co/WnjF6Tp] - Получаем и выводим весь список контактов в виде
+  таблицы
+- [https://ibb.co/hXzTzXj] - Получаем контакт по id
+- [https://ibb.co/tctjTGF] - Добавялем контакт
+- [https://ibb.co/GWH8y3T] - Удаляем контакт
--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Hello Node.js world!!!');
+const { program } = require('commander');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require('./contacts.js');
+
+program
+  .option('-a, --action <type>')
+  .option('-i, --id <type>')
+  .option('-n, --name <type>')
+  .option('-p, --phone <type>')
+  .option('-e, --email <type>');
+
+program.parse(process.argv);
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const allContacts = await listContacts();
+      console.table(allContacts);
+      break;
+
+    case 'get':
+      const contactById = await getContactById(id);
+      console.log(contactById);
+      break;
+
+    case 'add':
+      const addedContact = await addContact(name, email, phone);
+      console.log(addedContact);
+      break;
+
+    case 'remove':
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(options);
